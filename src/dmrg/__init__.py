e8fa1403f5729707f99760056050d8a5dcb189e4
SparseTrace <jcandane@proton.me>
2026-03-31 13:53:06 -0700

    Update __init__.py

--- src/dmrg/__init__.py
+++ src/dmrg/__init__.py
@@ -1,4 +1,5 @@
 from .DMRG import DMRG
 from .DiffusionBlock import DiffusionBlock
+from .VIT_evaluate import VIT_metrics
 
-__all__ = ["DMRG", "DiffusionBlock"]
+__all__ = ["DMRG", "DiffusionBlock", "VIT_metrics"]
