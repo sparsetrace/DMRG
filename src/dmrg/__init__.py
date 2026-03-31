2146de965daae5dce3c9bc5cda6a1cea20c3de80
SparseTrace <jcandane@proton.me>
2026-03-31 16:26:05 -0700

    Update __init__.py

--- src/dmrg/__init__.py
+++ src/dmrg/__init__.py
@@ -1,5 +1,6 @@
 from .DMRG import DMRG
 from .DiffusionBlock import DiffusionBlock
 from .VIT_evaluate import VIT_metrics
+from .login_token import login_token
 
-__all__ = ["DMRG", "DiffusionBlock", "VIT_metrics"]
+__all__ = ["DMRG", "DiffusionBlock", "VIT_metrics", "login_token"]
